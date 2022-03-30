67e315f448136dea2e1d250be04fa74b21672097
Szarvas Péter <peterszarvas94@gmail.com>
2022-03-30 21:19:35 +0200

    first break

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 
 const main = async () => {
+	//@ts-ignore
 	const orm = await MikroORM.init({
+		entities: [],
 		dbName: "lireddit",
 		user: "postgres",
 		password: "postgres",
