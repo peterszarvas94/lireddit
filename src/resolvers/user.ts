1e11d751fc44c64a08ceba0c269eaf94de53469b
Szarvas Péter <peterszarvas94@gmail.com>
2022-04-05 21:20:15 +0200

    login empty username and password check

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -39,18 +39,42 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-	@Mutation(() => User)
+	@Mutation(() => UserResponse)
 	async regitster(
 		@Arg("options") options: UsernamePasswordInput,
 		@Ctx() { em }: MyContext
-	) {
+	): Promise<UserResponse> {
+		if (options.username.length <= 2) {
+			return {
+				errors: [
+					{
+						field: "username",
+						message: "length must be greated than 2",
+					},
+				],
+			};
+		}
+
+		if (options.password.length <= 2) {
+			return {
+				errors: [
+					{
+						field: "password",
+						message: "length must be greated than 2",
+					},
+				],
+			};
+		}
+
 		const hashedPassword = await argon2.hash(options.password);
 		const user = em.create(User, {
 			username: options.username,
 			password: hashedPassword,
 		});
 		await em.persistAndFlush(user);
-		return user;
+		return {
+			user,
+		};
 	}
 
 	@Mutation(() => UserResponse)
