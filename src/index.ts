3df2e0dfa24996064be7992ead20f6e077b4d116
Szarvas Péter <peterszarvas94@gmail.com>
2022-04-06 22:54:47 +0200

    redis added

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,48 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
-import { MyContext } from "./types";
 import { UserResolver } from "./resolvers/user";
+import { createClient } from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { __prod__ } from "./constants";
+import { MyContext } from "./types";
 
 const main = async () => {
 	const orm = await MikroORM.init(mikroConfig);
 	orm.getMigrator().up();
 
 	const app = express();
+	const RedisStore = connectRedis(session);
+
+	const redisClient = createClient({ legacyMode: true });
+	redisClient.connect().catch(console.error)
+
+	app.use(
+		session({
+			name: "qid",
+			store: new RedisStore({
+				client: redisClient,
+				disableTouch: true,
+			}),
+			cookie: {
+				maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+				httpOnly: true,
+				sameSite: "lax", // csrf
+				secure: __prod__, //cookie only works in https
+			},
+			secret: "qwizhieuafbkjdnvoisdksowesd",
+			resave: false,
+			saveUninitialized: true
+		})
+	);
 
 	const apolloServer = new ApolloServer({
 		schema: await buildSchema({
 			resolvers: [HelloResolver, PostResolver, UserResolver],
 			validate: false,
 		}),
-		context: (): MyContext => ({ em: orm.em.fork() }),
+		context: ({req, res}) : MyContext => ({ em: orm.em.fork(), req, res }),
 	});
 
 	await apolloServer.start();
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
 import { EntityManager } from "@mikro-orm/core"
+import { Request, Response } from "express"
 
 export type MyContext = {
-  em: EntityManager
+  em: EntityManager;
+  req: Request;
+  res: Response;
 }
