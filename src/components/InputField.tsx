8abdf04713d86a7b99e7d97ed4672abdf3772790
Szarvas Péter <peterszarvas94@gmail.com>
2022-04-19 21:34:14 +0200

    register page styling

---
+++ src/components/InputField.tsx
@@ -0,0 +1,31 @@
+import {
+	FormControl,
+	FormLabel,
+	Input,
+	FormErrorMessage,
+} from "@chakra-ui/react";
+import { FieldHookConfig, useField } from "formik";
+import React, { InputHTMLAttributes } from "react";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+	label: string;
+	name: string;
+};
+
+const InputField = ({ label, size: _, ...props }: InputFieldProps) => {
+	const [field, { error }] = useField(props);
+
+	return (
+		<FormControl isInvalid={!!error}>
+			<FormLabel htmlFor={field.name}>{label}</FormLabel>
+			<Input
+				{...field}
+				{...props}
+				id={field.name}
+				placeholder={props.placeholder}
+			/>
+			{error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+		</FormControl>
+	);
+};
+export default InputField;
